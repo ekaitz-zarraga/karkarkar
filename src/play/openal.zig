f4441bfaf1e1ec83f5734492cfeccd4eb8eea0d2
Ekaitz Zarraga <ekaitz@elenq.tech>
2023-11-30 19:07:22 +0100

    openal: fix sample count issue

--- src/play/openal.zig
+++ src/play/openal.zig
@@ -56,7 +56,7 @@ pub const OpenAl = struct {
             return OpenAlError.GenSources;
         }
         c.alBufferData(buffer, c.AL_FORMAT_MONO16, samples.ptr,
-            @truncate(u16, samples.len*@sizeOf(c_short)), 16000);
+            @bitCast(i32, @truncate(u32, samples.len*@sizeOf(c_short))), 16000);
         if (c.alGetError() != c.AL_NO_ERROR){
             return OpenAlError.BufferData;
         }
